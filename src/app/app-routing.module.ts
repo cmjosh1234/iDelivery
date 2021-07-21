2cce6964ea9f5b81e8d57f82452706d180ebfccb
CHELANGAT MARVIN JOSHUA <mavinjcm@gmail.com>
2021-07-21 12:09:12 +0300

    21/07/2021

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { SignupComponent } from './pages/signup/signup.component';
 import { ShopsComponent } from './pages/shops/shops.component';
 import { AddshopComponent } from './pages/addshop/addshop.component';
-
-
 
 
 
 const routes: Routes = [
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: '',redirectTo: 'login',pathMatch: 'full'},
-  { path: 'login', component:LoginComponent },
-  { path: 'signup', component: SignupComponent },
   { path: 'shops', component:ShopsComponent },
   {path:'addshop',component:AddshopComponent},
+  
   {
     path: 'tabs',
     loadChildren: () => import('./pages/tab-bars/tab-bars.module').then( m => m.TabBarsPageModule)
@@ -29,16 +24,24 @@ const routes: Routes = [
     loadChildren: () => import('./pages/aboutshop/aboutshop.module').then( m => m.AboutshopPageModule)
   },
   {
-    path: 'orders',
-    loadChildren: () => import('./pages/orders/orders.module').then( m => m.OrdersPageModule)
-  },
-  {
     path: 'products',
     loadChildren: () => import('./pages/products/products.module').then( m => m.ProductsPageModule)
   },
   {
+    path: 'orders',
+    loadChildren: () => import('./pages/orders/orders.module').then( m => m.OrdersPageModule)
+  },
+  {
     path: 'shop',
     loadChildren: () => import('./pages/shop/shop.module').then( m => m.ShopPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   }
 
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+  apiKey: "AIzaSyAgiw9bdbiXTWk_AvJTYiuQUGQyKhgfRCo",
+  authDomain: "idelivery-f27b0.firebaseapp.com",
+  databaseURL: "https://idelivery-f27b0-default-rtdb.firebaseio.com",
+  projectId: "idelivery-f27b0",
+  storageBucket: "idelivery-f27b0.appspot.com",
+  messagingSenderId: "436082587880",
+  appId: "1:436082587880:web:f51da70331edea8b6cb15d",
+  measurementId: "G-LN2V6P4N27"
+  }
+
 };
 
 /*
